3c55a6357d3539f1fa80d903ab7ae99e732dfad6
n-fia <78014168+n-fia@users.noreply.github.com>
2025-06-05 09:23:58 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.12 ms, Memory: 3.71 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    for(int i=0; i<n; ++i){
+        (i&1 ? answer+="박" : answer+="수");
+    }
+    return answer;
+}
