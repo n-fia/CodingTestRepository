5cc468ee25a41d82af924d914e2eccaf3ea35301
n-fia <78014168+n-fia@users.noreply.github.com>
2025-06-16 09:38:21 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.05 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
@@ -1,74 +1,16 @@
 #include <string>
 #include <vector>
+#include <regex>
 
 using namespace std;
 
 int solution(string s) {
-    string answer = "";
-    //map
-    //switch-case
-    for(int i=0;i<s.length();++i){
-        switch(s[i]) {
-        case 122 :
-            answer+='0';
-            i+=3;
-            break;
-        
-        case 111 :
-            answer+='1';
-            i+=2;
-            break;            
-        
-        case 116 :
-            if(s[i+1]=='w'){
-                answer+='2';
-                i+=2;
-                break;
-            }
-            else{
-                answer+='3';
-                i+=4;
-                break;   
-            }
-        
-        case 102 :
-            if(s[i+1]=='o'){
-                answer+='4';
-                i+=3;
-                break;   
-            }
-            else{
-                answer+='5';
-                i+=3;
-                break;   
-            }
-    
-        case 115:
-            if(s[i+1]=='i'){
-                answer+='6';
-                i+=2;
-                break;   
-            }
-            else{
-                answer+='7';
-                i+=4;
-                break;   
-            }
-        
-        case 101 : 
-            answer+='8';
-            i+=4;
-            break;            
-        
-        case 110 : 
-            answer+='9';
-            i+=3;
-            break;            
-        
-        
-        default :
-            answer+=s[i];
-        }
+    string number[] = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+    int i=0;
+    for(string str : number){
+        regex re(str);
+        s=regex_replace(s,re,to_string(i));
+        ++i;
     }
-    return stoi(answer);
+    return stoi(s);
 }
