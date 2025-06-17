45f5ddd7608754a9800c2e409c689852cfc12a5c
n-fia <78014168+n-fia@users.noreply.github.com>
2025-06-17 09:54:37 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.06 ms, Memory: 3.69 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.cpp
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.cpp
@@ -1,28 +1,14 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 vector<string> solution(vector<string> strings, int n) {
-    string temp;
-    for(int j=1; j<strings.size();++j){
-        for(int i=0; i<strings.size()-j;++i){
-            int m=-1;
-            char str1 = strings[i][n];
-            char str2 = strings[i+1][n];
-        while(str1==str2){
-            ++m;
-            if(m==strings[i].length()){break;}
-            str1 = strings[i][m];
-            str2 = strings[i+1][m];
-        }
-        if(str1>str2){
-            temp=strings[i+1];
-            strings[i+1]=strings[i];
-            strings[i]=temp;
-        }
-        }
-    }
+    sort(strings.begin(), strings.end(),[n](string a, string b){
+        if(a[n]==b[n]){return a<b;}
+        return a[n]<b[n];
+    });
     
     return strings;
 }
