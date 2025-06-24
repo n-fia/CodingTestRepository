364b472c7778b025d26f187d300c82c169b24a74
n-fia <78014168+n-fia@users.noreply.github.com>
2025-06-24 13:16:36 +0900

    [level 1] Title: 행렬의 덧셈, Time: 56.87 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.cpp
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.cpp
@@ -4,10 +4,16 @@
 using namespace std;
 
 vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
-    for(size_t i=0; i<arr1.size();++i){
-        for(size_t j=0; j<arr1[i].size();++j){
-            arr1[i][j]+=arr2[i][j];
+    //arr1.size() , vector<int>(arr1[0].size()) 
+    vector<vector<int>> answer(arr1.size());
+    
+    for(size_t i=0; i<arr1.size(); ++i)
+    {
+        for(size_t j=0; j<arr1[i].size();++j)
+        {
+            //행에 순서대로 넣는 방법
+            answer[i].push_back(arr1[i][j] + arr2[i][j]);
         }
     }
-    return arr1;
+    return answer;
 }
