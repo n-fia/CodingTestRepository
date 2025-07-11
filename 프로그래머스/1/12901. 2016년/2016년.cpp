898a0ad26d5871f67688e9a002a74476a3484a91
n-fia <78014168+n-fia@users.noreply.github.com>
2025-07-11 09:29:31 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 3.74 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.cpp
+++ 프로그래머스/1/12901. 2016년/2016년.cpp
@@ -4,18 +4,11 @@
 using namespace std;
 
 string solution(int a, int b) {
-    int x=0;
-    string str[] = { "THU","FRI","SAT","SUN","MON","TUE","WED" };
-    int arr[]={31,29,31,30,31,30,31,31,30,31,30,31};
-    // 1월 1일 금요일
-    // 1월 8일 금요일(1+7)
-    // 1월은 31일까지
-    // 1월 31일은 몇요일이지 > 7*4+3 > 금 토 일요일
-    // 2월은 29일 > 31+29 > 60 > 7*8 + 4 > 금 토 일 월 !
-    for (int i=0; i<a-1 ; ++i){
-        x+=arr[i];
+    vector<string> days{"THU","FRI","SAT","SUN","MON","TUE","WED"};
+    vector<int> month{31,29,31,30,31,30,31,31,30,31,30,31};
+    int num=0;
+    for(int i=0; i<a-1;++i){
+        num+=month[i];
     }
-    x=(x+b)%7;
-    
-    return str[x];
+    return days[(num+b)%7];
 }
