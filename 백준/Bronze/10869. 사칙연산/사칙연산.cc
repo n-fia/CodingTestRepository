5d78e88c6580752fc8042698b1aa044673226dd9
n-fia <78014168+n-fia@users.noreply.github.com>
2025-07-31 09:28:18 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int A, B;
+	cin>>A;
+	cin>>B;
+
+	cout<<A+B<<endl;
+	cout<<A-B<<endl;
+	cout<<A*B<<endl;
+	cout<<A/B<<endl;
+	cout<<A%B<<endl;
+}
