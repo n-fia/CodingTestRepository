d202b59f13b6bc3308bf256a560bd5d3cf092830
n-fia <78014168+n-fia@users.noreply.github.com>
2025-07-31 09:46:03 +0900

    [Bronze V] Title: 나머지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int A,B,C;
+	cin>>A>>B>>C;
+	cout<<(A+B)%C<<endl;
+	cout<<((A%C) + (B%C))%C<<endl;
+	cout<<(A*B)%C<<endl;
+	cout<<((A%C) * (B%C))%C<<endl;
+}
