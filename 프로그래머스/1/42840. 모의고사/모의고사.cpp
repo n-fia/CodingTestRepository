2c84580536de00422394af5bb97bd4ca84ba3fbb
n-fia <78014168+n-fia@users.noreply.github.com>
2025-08-28 10:45:00 +0900

    [level 1] Title: 모의고사, Time: 0.13 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -5,30 +5,21 @@
 using namespace std;
 
 vector<int> solution(vector<int> answers) {
-    vector<int> answer(3);
-    int a=0,b=0,c=0;
+    vector<int> answer;
     int arr1[] = {1, 2, 3, 4, 5};
     int arr2[] = {2, 1, 2, 3, 2, 4, 2, 5};
     int arr3[] = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-    for(int i=0;i<answers.size();++i){
-        if( answers[i] == arr1[i%5]){
-            ++answer[0];
-        }
-        if( answers[i] == arr2[i%8] ){
-            ++answer[1];
-        }
-        if( answers[i] == arr3[i%10] ){
-            ++answer[2];
-        }
-    }
-    vector<int> answer2;
-    int maxNum = max({answer[0], answer[1], answer[2]});
-    for(int i=0; i<answer.size(); ++i){
-        if(answer[i]==maxNum){
-            answer2.push_back(i+1);
-        }
+    vector<int> arr(3,0);
+    for(int i=0; i<answers.size(); ++i){
+        if(arr1[i%5] == answers[i]) ++arr[0];
+        if(arr2[i%8] == answers[i]) ++arr[1];
+        if(arr3[i%10] == answers[i]) ++arr[2];
     }
     
-//     answer.erase(answer.begin(), answer.begin()+3);
-    return answer2;
+    int num = *max_element(arr.begin(), arr.end());
+    if(arr[0]==num) answer.push_back(1);
+    if(arr[1]==num) answer.push_back(2);
+    if(arr[2]==num) answer.push_back(3);
+    
+    return answer;
 }
